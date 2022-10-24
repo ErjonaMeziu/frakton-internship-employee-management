5ab34aeb5a9c3edbd58c480034d512064e3d6683
Erjona Meziu <erjona.mziu@frakton.com>
2022-10-24 16:31:52 +0200

    Add prisma migrate and store some data to database

---
+++ prisma/migrations/20221024123758_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('PlatformAdmin', 'CompanyOwner', 'CompanyAdmins');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "register_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" "Role" NOT NULL,
+    "profile" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "joined_at" TIMESTAMP(3) NOT NULL,
+    "updatet_at" TIMESTAMP(3) NOT NULL,
+    "logo" TEXT,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "hired_at" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "companyId" INTEGER NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_name_key" ON "Company"("name");
+
+-- AddForeignKey
+ALTER TABLE "Company" ADD CONSTRAINT "Company_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/services/Ping.service.ts
+++ src/services/Ping.service.ts
@@ -1,5 +1,15 @@
+import { prisma }  from '../config/prisma';
+
 export const PingService = {
     test: async () => {
-        return 'Pong';
+       await prisma.user.create({
+           data: {
+               name: 'John Doe',
+               email: 'jondoe@gmail.com',
+               register_at: "2022-03-22T16:45:01.246Z",
+               role: 'CompanyAdmins'
+           },
+       });
+        
     },
 };
