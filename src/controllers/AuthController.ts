0e749dbbe3c45ee5bf08c6bad7491783f5b9f0b5
Erjona Meziu <erjona.mziu@frakton.com>
2022-11-01 12:27:59 +0100

    Add input validation with joi

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,18 +1,17 @@
-import { NextFunction, Request, Response, Router } from 'express';
-
-import { RegisterService } from '../services/RegisterService';
+import { NextFunction, request, Request, Response, Router } from 'express';
+import { PingService } from '../services/Ping.service';
+import { RegisterService } from '../services/registerService';
 import { LoginService } from '../services/LoginService';
-import { schema } from '../validators/userInputValidator';
+
+
 import Joi from 'joi';
+
 
 export const AuthController: Router = Router();
 
 AuthController.post('/register', async (req: Request, res: Response, next: NextFunction) => {
     const { userName, userEmail, password, companyName } = req.body;
-    try
-    {
-        
-    
+    try {
         const result = await RegisterService.register(userName, userEmail, password, companyName);
 
         res.status(result.status).send({
@@ -36,4 +35,19 @@ AuthController.post('/login', async (req: Request, res: Response, next: NextFunc
         next(e);
     }
 });
+
+AuthController.post('/login', async (req: Request, res: Response, next: NextFunction) => {
+    const { userEmail, password } = req.body;
+    try {
+        const result = await LoginService.login(userEmail, password);
+
+        res.status(result.status).send({
+            data: result.data,
+        });
+    } catch (e) {
+        next(e);
+    }
+});
+
+
 
