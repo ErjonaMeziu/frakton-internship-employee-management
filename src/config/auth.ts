21abb879c5975856c78cd90488fdb1c56db3e976
Erjona Meziu <erjona.mziu@frakton.com>
2022-11-01 13:42:07 +0100

    Add authentication logic

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,5 +2,5 @@ import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
 export const JWT_SECRET: Secret =
     process.env.JWT_SECRET ||
     'a2715dabe313c2935ebe875f43b274619298a5f876cd3b5ddea133ab0410a75253fb29ab03bb35b0f51485ab06a258176b9f7cad1efacd3facadf414909f21e1';
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '30s';
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '14d';
 export const JWT_ALGORITHM = process.env.JWT_ALGORITHM || 'HS256';
--- src/utils/comparePassword.util.ts
+++ src/utils/comparePassword.util.ts
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt';
 
 export const ComparePassword = async (password: string, hashedPassword: string) => {
     return new Promise((resolve, reject) => {
