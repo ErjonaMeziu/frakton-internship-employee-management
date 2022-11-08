dd64c1003b4678ff9671cf1d2a59bf39bd5c2d50
Erjona Meziu <erjona.mziu@frakton.com>
2022-11-08 14:44:27 +0100

    Add condition for company registration

--- src/services/registerService.ts
+++ src/services/registerService.ts
@@ -6,35 +6,46 @@ import { Event } from '../events/App.event'
 export const RegisterService = {
     register: async (userName: string, userEmail: string, password: string, companyName: string,logo:string) => 
     {
-        
-        const hashedPassword = await HashPassword(password);
-        const userData = await prisma.user.create({
-            data: {
-                name: userName,
-                email: userEmail,
-                password:hashedPassword,
-                register_at: new Date(),
-                role: 'CompanyOwner',
-                is_approved: false,
-            },
-        });
-        const companyData = await prisma.company.create({
-            data: {
-                userId: userData.id,
-                name: companyName,
-                joined_at: new Date(),
-                updated_at: new Date(),
-                status:"inactive",
-                logo:logo,
-            },
-        });
-        const data = {
-            companyOwner: userData.name,
-            companyName:companyData.name,
+        const comp=await prisma.company.findFirst({
+            where: {
+                name:companyName
+            }
+        })
+        if (!comp)
+        {
+            const hashedPassword = await HashPassword(password);
+            const userData = await prisma.user.create({
+                data: {
+                    name: userName,
+                    email: userEmail,
+                    password: hashedPassword,
+                    register_at: new Date(),
+                    role: 'CompanyOwner',
+                    is_approved: false,
+                },
+            });
+            const companyData = await prisma.company.create({
+                data: {
+                    userId: userData.id,
+                    name: companyName,
+                    joined_at: new Date(),
+                    updated_at: new Date(),
+                    status: "inactive",
+                    logo: logo,
+                },
+            });
+
+            const data = {
+                companyOwner: userData.name,
+                companyName: companyData.name,
+            }
+            Event.emit('register::company', (data));
+            return { status: 200, data: "Your request to join our platform has been successful." };
         }
-        Event.emit('register::company', (data));
-        return { status: 200, data: "Your request to join our platform has been successful." };
-        
+        else
+        {
+            return { status: 409, data: "You cannot register company twice" };
+        }
     },
    
 };
